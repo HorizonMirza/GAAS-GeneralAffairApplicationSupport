1e33ed976fea000757bc6b6e88c9042b09ece31a
Muhammad Mirza Wirya <purbandonomirza@gmail.com>
2026-08-13 09:02:10 +0700

    Filter Direktorat/Divisi/Departemen bertingkat

--- api/obj/Debug/net8.0/PengirimanApi.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/PengirimanApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PengirimanApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a6d4251d79478cc387355f2eb64a21133019a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd320418236e9c94cf718dfe5f924377aa89372c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PengirimanApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PengirimanApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
